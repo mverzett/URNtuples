c7e221ac0c72d727f30507510129bdd9e881dbfe
mauro verzetti <mverzett@cern.ch>
2016-09-02 09:15:07 -0500

    fixed naming for local environment

--- Configuration/site/site_configuration.sh
+++ Configuration/site/site_configuration.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 site=`dnsdomainname`
-if [ -e $URA/Configuration/site/$site/environment.sh ]; then
-    source $URA/Configuration/site/$site/environment.sh 
+if [ -e $URN/Configuration/site/$site/environment.sh ]; then
+    source $URN/Configuration/site/$site/environment.sh 
 else
     echo "Domain name $site not available in the list of supported sites."
     echo "Change site or add the proper configuration in Configuration/site/$dnsdomainname"
