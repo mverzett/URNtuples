6f98646828df4193d8f0aaab724d28b65a19165e
mauro verzetti <mverzett@cern.ch>
2016-09-06 10:14:49 -0500

    lower muon min pt threshold to 8 GeV

--- PATTools/python/objects/muons.py
+++ PATTools/python/objects/muons.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 urSkimmedMuons = cms.EDFilter(
     "PATMuonSelector",
     src = cms.InputTag("fixme"),
-    cut = cms.string('pt > 15 && abs(eta) < 2.5 && (isGlobalMuon || isTrackerMuon)')
+    cut = cms.string('pt > 8 && abs(eta) < 2.5 && (isGlobalMuon || isTrackerMuon)')
 )
 customMuons = cms.Sequence(urSkimmedMuons)
 
